e58884aeb31fd44abdcf062989eba2e093eae76b
anyaxkar <anyakar.kar@yandex.ru>
2023-07-06 16:32:47 +0300

    Исправление решения 11 задачи для работы с битами.

--- 1.4/src/main/java/filterOfBlume/BloomFilter.java
+++ 1.4/src/main/java/filterOfBlume/BloomFilter.java
@@ -3,11 +3,11 @@ package filterOfBlume;
 public class BloomFilter {
 
     private final int filter_len;
-    private final boolean[] bits;
+    private int bits;
 
     public BloomFilter(int f_len) {
         this.filter_len = f_len;
-        this.bits = new boolean[this.filter_len];
+        this.bits = 0;
         // создаём битовый массив длиной f_len ...
     }
 
@@ -31,8 +31,8 @@ public class BloomFilter {
     public void add(String str1) {
         final int hash1 = this.hash1(str1);
         final int hash2 = this.hash2(str1);
-        this.bits[hash1] = true;
-        this.bits[hash2] = true;
+        this.bits = this.bits | (1 << hash1);
+        this.bits = this.bits | (1 << hash2);
         // добавляем строку str1 в фильтр
     }
 
@@ -40,6 +40,7 @@ public class BloomFilter {
         // проверка, имеется ли строка str1 в фильтре
         final int hash1 = this.hash1(str1);
         final int hash2 = this.hash2(str1);
-        return this.bits[hash1] && this.bits[hash2];
+        return (this.bits & (1 << hash1)) == (1 << hash1)
+                && (this.bits & (1 << hash2)) == (1 << hash2);
     }
 }
